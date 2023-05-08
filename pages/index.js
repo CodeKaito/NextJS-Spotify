0e9c0534da672fd076d9f94bac5b15bcd353e7a3
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-08 17:13:31 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,9 @@
 import Sidebar from "@/components/Sidebar";
-import { useSession } from "next-auth/react"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 export default function Home() {
 
-  const {data:session} = useSession()
-  const [x, setX] = useState('');
-  const [playlists, setPlaylists ] = useState([]);
-
-  useEffect(() => {
-    async function f() {
-      if (session && session.accessToken) {
-        setX(session.accessToken)
-        const response = await fetch("https://api.spotify.com/v1/me/playlists", {
-          headers: {
-            Authorization: `Bearer ${session.accessToken}`
-          }
-        })
-        const data = await response.json()
-        setPlaylists(data.items)
-      }
-    }
-    f();
-  }, [session])
+  const [view, setView] = useState("Search"); // Search Library
 
   return (
     <>
