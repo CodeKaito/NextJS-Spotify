b94e13c25a28f4928304dedc71fbd785af26d09d
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-08 17:24:31 +0200

    update

--- components/PlaylistView.jsx
+++ components/PlaylistView.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function PlaylistView() {
   return (
-    <div>
-      
+    <div className='fle-frow h-screen bg-neutral-800'>
+        Playlist View
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
+import PlaylistView from "@/components/PlaylistView";
 import Sidebar from "@/components/Sidebar";
 import { useState } from "react"
 
 export default function Home() {
 
   const [view, setView] = useState("Search"); // Search Library
+  const [globalPlaylistId, setGlobalPlaylistId] = useState(null);
+  const [globalArtistId, setGlobalArtistId] = useState(null);
 
   return (
     <>
       <main className="flex w-full h-screen overflow-hidden bg-black">
-        <Sidebar />
-        <div>Main</div>
+        <Sidebar
+          view={view}
+          setView={setView} 
+          setGlobalPlaylistId={setGlobalPlaylistId}
+          />
+        {view === "playlist" && <PlaylistView />}
       </main>
     <div className="sticky z-20 bottom-0 h-24 w-full bg-red-200">Player</div>
     </>
