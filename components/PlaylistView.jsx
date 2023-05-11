32ca986dc5530b14249ffda89bc702a21dae5cd4
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-05-11 09:27:20 +0200

    update

--- components/PlaylistView.jsx
+++ components/PlaylistView.jsx
@@ -42,15 +42,15 @@ function PlaylistView({ globalPlaylistId }) {
   
   return (
     <div className='flex-grow h-screen'>
-        <header className='text-white sticky top-0 h-20 z-10 text-4xl bg-neutral-800 p-8 flex items-center font-bold'>
-          <div>{playlistData?.name}</div>
+        <header style={{opacity: opacity}} className='text-white sticky top-0 h-20 z-10 text-4xl bg-neutral-800 p-8 flex items-center font-bold'>
+          <div style={{opacity: textOpacity}}>{playlistData?.name}</div>
         </header>
         <div className='absolute z-20 top-5 right-8 flex items-center bg-black bg-opacity-70 text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
           <img className='rounded-full w-7 h-7' src={session?.user.image} alt='profile pic'/>
           <p className='text-sm'>Logout</p>
           <ChevronDownIcon className='h-5 w-5'/>
         </div>
-        <div className='relative -top-20 h-screen overflow-y-scroll bg-neutral-900'>
+        <div onScroll={(e) => changeOpacity(e.target.scrollTop)} className='relative -top-20 h-screen overflow-y-scroll bg-neutral-900'>
           <section className={`flex items-end space-x-7 bg-gradient-to-b to-neutral-900 ${color} h-80 text-white p-8`}>
             {playlistData && <img className='h-44 w-44' src={playlistData.images[0].url} alt="" />}
             <div>
@@ -61,7 +61,7 @@ function PlaylistView({ globalPlaylistId }) {
           <div className='text-white px-8 flex flex-col space-y-1 pb-28'>
             {playlistData?.tracks.items.map((track, i) => {
               // song component
-              return <div key={track.track.id}></div>
+              return <div key={track.track.id}>{track.track.name}</div>
             })}
           </div>
         </div>
